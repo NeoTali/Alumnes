5d83d5e5155f3eeb6ee39b7e40bb266333a83621
Unknown <perlesofertes@gmail.com>
2017-11-03 23:52:09 +0100

    Creació del gruntfile
    ToDo: Make the watch!

--- DetallController.js
+++ DetallController.js
@@ -1,3 +1,5 @@
+var app = angular.module('app',[]);
+
 app.controller('DetallController',["$scope", "AlumnesFactory", function($scope, AlumnesFactory){
     vm = $scope;
     vm.alm = AlumnesFactory.getAlumne();
